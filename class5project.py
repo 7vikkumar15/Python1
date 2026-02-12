1864c112bf43344569f56ee21c603af2bad2c8e1
7vikkumar15 <7vik.kumar15@gmail.com>
2026-02-12 12:26:32 +0530

    Add files via upload

---
+++ class5project.py
@@ -0,0 +1,9 @@
+# Conditional statement example – Summer time
+
+month = int(input("Enter month number (1-12): "))
+
+# assuming summer = March to June
+if month >= 3 and month <= 6:
+    print("It is summer season ☀️")
+else:
+    print("It is not summer season")
