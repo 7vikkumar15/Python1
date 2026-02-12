a56dc6765d5bd145cc686a5a452806f63e3b6770
7vikkumar15 <7vik.kumar15@gmail.com>
2026-02-12 12:24:26 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,66 @@
+'''
+Welcome to the world of coding
+Outline:
+Write a program for print command.
+'''
+
+'''
+print('Welcome to the world of programming')
+
+print(4)
+
+print('Hello' , 4)
+
+print('My name is Saatvik.\n I am 13 years old ')
+
+print('Welcome to ' , end='*')
+'''
+
+
+
+
+#Activity 2
+
+'''
+Understanding the variables
+Outline:
+Write a program to create different variables to store various values.
+
+
+I am using lenovo laptop
+
+
+car='saatvik'
+print('My name is', car )
+
+                                
+laptop='lenovo'
+
+print('I am using', laptop , 'laptop')
+
+x='john'
+
+y = 5
+
+z= 6.5
+
+a= True
+
+
+name=input('Enter Your name  ')
+print(name)
+
+'''
+
+# Activity 3
+
+'''Keywords
+Outline:
+Write a program to print all the keywords
+'''
+
+
+#import keyword
+ #print(keyword.kwlist)
+name=input('Enter Your name')
+print(name)
