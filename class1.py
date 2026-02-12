ca2f73eaf0cdda168e699800cf802b89deb915e6
7vikkumar15 <7vik.kumar15@gmail.com>
2026-02-12 12:24:00 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,17 @@
+# Printing a greeting message
+
+print("Hello World")
+
+
+# Printing my name 
+
+print("My name is Saatvik")
+
+
+# Print a fun fact about yourself
+
+print("I love playing Lawn Tennis")
+
+# Performing and printing a simple calculation
+
+print('The result of 9+2 is :' , 9+2)
