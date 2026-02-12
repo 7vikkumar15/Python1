7d673253e62dd3efbfdb718c6471fc93c2c0ee71
7vikkumar15 <7vik.kumar15@gmail.com>
2026-02-12 12:24:45 +0530

    Add files via upload

---
+++ class2project.py
@@ -0,0 +1,14 @@
+# storing birthdays in different variables
+
+birthday1 = "12-03-2005"
+birthday2 = "25-07-2006"
+birthday3 = "09-11-2004"
+birthday4 = "18-01-2005"
+birthday5 = "30-12-2006"
+
+# printing them
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
