b7239fb4fde37cddac35ae79d0431abdb83e9854
7vikkumar15 <7vik.kumar15@gmail.com>
2026-03-12 09:27:36 +0530

    Add files via upload

---
+++ class9project.py
@@ -0,0 +1,10 @@
+# Program to count total digits in a number
+
+num = int(input("Enter a number: "))
+count = 0
+
+while num != 0:
+    num = num // 10
+    count = count + 1
+
+print("Total digits:", count)
