db4e6b9d8a2054a360f1776430afd68f7f31c7d5
7vikkumar15 <7vik.kumar15@gmail.com>
2026-03-12 09:25:33 +0530

    Add files via upload

---
+++ class8project.py
@@ -0,0 +1,12 @@
+'''Power calculator'''
+
+# Power Calculator
+
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+
+result = base ** exponent
+
+print("Result:", result)
+
+
