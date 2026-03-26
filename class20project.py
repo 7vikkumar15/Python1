11eed257061d1a53198e6cc3b5a864f7164d3ac5
7vikkumar15 <7vik.kumar15@gmail.com>
2026-03-26 11:14:33 +0530

    Add files via upload

---
+++ class20project.py
@@ -0,0 +1,12 @@
+# Define the tuple
+numbers = (2, 3, 4, 5)
+
+# Initialize product
+product = 1
+
+# Loop through the tuple
+for num in numbers:
+    product *= num
+
+# Display result
+print("Product of tuple elements:", product)
