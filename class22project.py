1402a94863633529e6fa7def579eb369b244be98
7vikkumar15 <7vik.kumar15@gmail.com>
2026-03-31 11:00:19 +0530

    Add files via upload

---
+++ class22project.py
@@ -0,0 +1,9 @@
+# Input sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Finding symmetric difference
+result = set1.symmetric_difference(set2)
+
+# Display result
+print("Symmetric Difference:", result)
