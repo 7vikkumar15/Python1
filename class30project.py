b8981a94f0f7826fd32808ddd56ed29cdc47448c
7vikkumar15 <7vik.kumar15@gmail.com>
2026-04-16 12:16:44 +0530

    Add files via upload

---
+++ class30project.py
@@ -0,0 +1,34 @@
+class IntegerToRoman:
+    def __init__(self, number):
+        self.number = number
+
+    def convert(self):
+        # Roman numeral mapping
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+        ]
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+
+        num = self.number
+        roman = ""
+
+        for i in range(len(val)):
+            while num >= val[i]:
+                roman += syms[i]
+                num -= val[i]
+
+        return roman
+
+
+# Example usage
+number = int(input("Enter an integer: "))
+obj = IntegerToRoman(number)
+print("Roman Numeral:", obj.convert())
