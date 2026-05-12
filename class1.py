27d73e6334615b7f713573f136db279c9c0f519f
7vikkumar15 <7vik.kumar15@gmail.com>
2026-05-12 12:11:43 +0530

    Add files via upload

--- class1.py
+++ class1.py
@@ -1,17 +1,51 @@
-# Printing a greeting message
 
-print("Hello World")
+'''
+from tkinter import *
+window = Tk()
+window.title("Demo Window")
+window.geometry("800x700")
+
+window.mainloop()
+'''
+
+#Import neccesary
+from tkinter import *
+from datetime import date
+#Create Window
+root = Tk()
+root.title('Getting Started with widgets')
+root.geometry('500x500')
+
+#Widgets
+lbl = Label(text = "Hey There!" , fg="white" , bg = "#07257F" , height = 1 , width = 300)
+
+#  Add label for name
+name_lbl = Label(text = "Full Name" , bg = "#3895D3")
+name_entry = Entry()
+
+lbl.pack()
+name_lbl.pack()
+name_entry.pack()
 
 
-# Printing my name 
+def display():
+    name = name_entry.get()
+    global Message
+    message = "Welcome to the Application! \n Today's date is: "
+    greet = "Hello "+name+"\n"
 
-print("My name is Saatvik")
+    text_box.insert(END , greet)
+    text_box.insert(END  , message)
+    text_box.insert(END  , date.today())
 
 
-# Print a fun fact about yourself
+#btn
+btn = Button(text = "Begin" , height=1 , bg = "#1261A0" ,command=display ,  fg = "white")
+btn.pack()
 
-print("I love playing Lawn Tennis")
+text_box = Text(height=3)
+text_box.pack()
 
-# Performing and printing a simple calculation
 
-print('The result of 9+2 is :' , 9+2)
+
+root.mainloop()
